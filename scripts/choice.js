106d0c84cec29a4d6223c094867057fa7f9246d1
mironovaEv <mironovaev26@gmail.com>
2023-02-02 19:23:34 +0700

    Улучшена реализация меню - добавлены async await в функцию с запросом -добавлена возможность выбирать категорию для пицц

--- scripts/choice.js
+++ scripts/choice.js
@@ -1,24 +1,46 @@
-$(document).ready(function () {
-  LoadMenu();
-});
-
-function LoadMenu() {
+async function loadMenu(category) {
   $('#pizza-items-container').empty();
-  fetch('https://shift-winter-2023-backend.onrender.com/api/pizza')
-    .then((response) => {
-      return response.json();
-    })
-    .then((json) => {
-      const template = $('#menu-item-template');
-      const pizzas = JSON.stringify(json);
-      for (const pizza of JSON.parse(pizzas)) {
-        const block = template.clone();
-        const ingredients = pizza.ingredients;
-        block.find('.pizza-name').text(pizza.name);
-        block.find('.pizza-price').text(pizza.price.default + ' ₽');
-        block.find('.pizza-img').attr('src', pizza.img);
-        block.removeClass('d-none');
-        $('#pizza-items-container').append(block);
-      }
-    });
+  const response = await fetch(
+    'https://shift-winter-2023-backend.onrender.com/api/pizza?' + new URLSearchParams({ classifications: category }),
+  );
+  const pizzas = await response.json();
+  const template = $('#menu-item-template');
+  for (const pizza of pizzas) {
+    const block = template.clone();
+    const ingredients = pizza.ingredients;
+    block.find('.pizza-name').text(pizza.name);
+    block.find('.pizza-price').text(pizza.price.default + ' ₽');
+    block.find('.pizza-img').attr('src', pizza.img);
+    block.find('.pizza-ingredients').text(ingredients.join(', '));
+    block.removeClass('d-none');
+    $('#pizza-items-container').append(block);
+  }
 }
+function deleteSelected() {
+  let selectedElems = document.getElementsByClassName('selected');
+  while (selectedElems.length) selectedElems[0].classList.remove('selected');
+}
+
+$(document).ready(function () {
+  loadMenu();
+  $('#new-button').click(function () {
+    deleteSelected();
+    $('#new-button').addClass('selected');
+    loadMenu('new');
+  });
+  $('#spicy-button').click(function () {
+    deleteSelected();
+    $('#spicy-button').addClass('selected');
+    loadMenu('spicy');
+  });
+  $('#vegetarian-button').click(function () {
+    deleteSelected();
+    $('#vegetarian-button').addClass('selected');
+    loadMenu('vegetarian');
+  });
+  $('#recommend-button').click(function () {
+    deleteSelected();
+    $('#recommend-button').addClass('selected');
+    loadMenu('');
+  });
+});
