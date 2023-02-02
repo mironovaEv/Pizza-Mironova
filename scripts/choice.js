2f692ab9eaac7fc1678e882e8e73c8e8b7345219
mironovaEv <mironovaev26@gmail.com>
2023-02-02 17:49:19 +0700

    Код отформатирован с помощью Prettier и Eslint

--- scripts/choice.js
+++ scripts/choice.js
@@ -1,25 +1,24 @@
-$(document).ready(function(){
-    LoadMenu();
+$(document).ready(function () {
+  LoadMenu();
 });
 
-function LoadMenu(){
-    $('#pizza-items-container').empty();
-    fetch('https://shift-winter-2023-backend.onrender.com/api/pizza')
-    .then((response) =>{
-        return response.json();
+function LoadMenu() {
+  $('#pizza-items-container').empty();
+  fetch('https://shift-winter-2023-backend.onrender.com/api/pizza')
+    .then((response) => {
+      return response.json();
     })
-    .then((json)=>{
-        let template = $("#menu-item-template");
-        let pizzas = JSON.stringify(json);
-        for (const pizza of JSON.parse(pizzas)){
-            let block = template.clone();
-            let ingredients= pizza.ingredients;
-            block.find(".pizza-name").text(pizza.name);
-            block.find(".pizza-price").text(pizza.price.default + " ₽");
-            block.find(".pizza-img").attr("src", pizza.img);
-            block.removeClass("d-none");
-            $("#pizza-items-container").append(block);
-        }
+    .then((json) => {
+      const template = $('#menu-item-template');
+      const pizzas = JSON.stringify(json);
+      for (const pizza of JSON.parse(pizzas)) {
+        const block = template.clone();
+        const ingredients = pizza.ingredients;
+        block.find('.pizza-name').text(pizza.name);
+        block.find('.pizza-price').text(pizza.price.default + ' ₽');
+        block.find('.pizza-img').attr('src', pizza.img);
+        block.removeClass('d-none');
+        $('#pizza-items-container').append(block);
+      }
     });
 }
-
